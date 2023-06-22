49c19dccab89e7c6f2097001f40c1481b8b3c5b9
Kevin Lam <38263881+lamkg@users.noreply.github.com>
2023-06-22 02:26:17 -0700

    Add files via upload
    hid the keys

--- src/api.js
+++ src/api.js
@@ -2,7 +2,7 @@ export const geoApiOptions = {
   method: 'GET',
   params: { countryIds: 'US' },
   headers: {
-    'X-RapidAPI-Key': 'process',
+    'X-RapidAPI-Key': '',
     'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com',
   },
 };
@@ -11,4 +11,4 @@ export const GEO_API_URL = 'https://wft-geo-db.p.rapidapi.com/v1/geo';
 
 export const WEATHER_API_URL = 'https://api.openweathermap.org/data/2.5';
 
-export const WEATHER_API_KEY = '509bf9a95a92443750e49b1ac4735edc';
+export const WEATHER_API_KEY = '';
