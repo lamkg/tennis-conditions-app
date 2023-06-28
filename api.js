de3cf51c52be5faa6e2c33e6bddfebbbbbbf069f
Kevin Lam <38263881+lamkg@users.noreply.github.com>
2023-06-27 23:08:06 -0700

    added dotenv config to hide my API keys

---
+++ api.js
@@ -0,0 +1,16 @@
+require('dotenv').config()
+
+export const geoApiOptions = {
+  method: 'GET',
+  params: { countryIds: 'US' },
+  headers: {
+    'X-RapidAPI-Key': process.env.X-RapidAPI-Key,
+    'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com',
+  },
+};
+
+export const GEO_API_URL = 'https://wft-geo-db.p.rapidapi.com/v1/geo';
+
+export const WEATHER_API_URL = 'https://api.openweathermap.org/data/2.5';
+
+export const WEATHER_API_KEY = process.env.WEATHER_API_KEY;
